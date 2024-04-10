0ddd4e9f0b9cc54c5c2843bffea7c5f63e14e12c
yusufdgzl <yusuf.dg13@gmail.com>
2024-04-10 08:17:02 +0300

    updated header -Responsive & navlink, feature of active

--- components/MainHeader.tsx
+++ components/MainHeader.tsx
@@ -1,20 +1,39 @@
+"use client";
+
 import logoImg from "@/assets/logo.png";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 function MainHeader() {
+  const pathName = usePathname();
+  const activeClass = "text-yellow-500 [text-shadow:_0_0_15px_yellow]";
+
   return (
-    <header className="flex relative bg-[url('https://images3.alphacoders.com/116/116517.jpg')] bg-cover  justify-between items-center px-36 py-8 font-bold text-white tracking-wider">
-      
-      <div className="flex  items-center space-x-6">
+    <header className="flex flex-col space-y-6 relative bg-[url('https://images3.alphacoders.com/116/116517.jpg')] bg-center md:bg-cover  justify-between items-center md:px-36 py-8 md:space-y-0 md:flex-row font-bold text-white tracking-wider ">
+      <div className="flex  items-center  space-x-6">
         <Image src={logoImg} className="w-20" alt="Main logo" />
         <Link className="text-2xl" href="/">
           NEXTLEVEL FOOD
         </Link>
       </div>
-      <nav className="space-x-10 text-xl font-semibold">
-        <Link href="meals">Browse Meals</Link>
-        <Link href="/community">Foodies Community</Link>
+      <nav className=" space-x-10  text-lg  md:text-xl font-semibold">
+        <Link
+          className={` hover:text-yellow-500 hover:[text-shadow:_0_0_15px_yellow]   px-3 py-1 rounded-full ${
+            pathName === "/meals" && activeClass
+          } `}
+          href="meals"
+        >
+          Browse Meals
+        </Link>
+        <Link
+          className={` hover:text-yellow-500 hover:[text-shadow:_0_0_15px_yellow]   px-3 py-1 rounded-full ${
+            pathName === "/community" && activeClass
+          } `}
+          href="/community"
+        >
+          Foodies Community
+        </Link>
       </nav>
     </header>
   );
