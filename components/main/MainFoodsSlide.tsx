8e0db6c171470065894ff90e214183f6ac2d5c92
yusufdgzl <yusuf.dg13@gmail.com>
2024-04-11 07:21:55 +0300

    component of MainFoodSlide added

--- components/main/MainFoodsSlide.tsx
+++ components/main/MainFoodsSlide.tsx
@@ -1,11 +1,52 @@
+"use client";
 
+import Image from "next/image";
+import burgerImg from "@/assets/burger.jpg";
+import curryImg from "@/assets/curry.jpg";
+import dumplingsImg from "@/assets/dumplings.jpg";
+import macncheeseImg from "@/assets/macncheese.jpg";
+import pizzaImg from "@/assets/pizza.jpg";
 
-function Slide(){
-    return(
-        <div>
+import tomatoSaladImg from "@/assets/tomato-salad.jpg";
+import { useEffect, useState } from "react";
 
-        </div>
-    )
+const allFoodsImage = [
+  { image: burgerImg, alt: "A delicious, juicy burger" },
+  { image: curryImg, alt: "A delicious, spicy curry" },
+  { image: dumplingsImg, alt: "Steamed dumplings" },
+  { image: macncheeseImg, alt: "Mac and cheese" },
+  { image: pizzaImg, alt: "A delicious pizza" },
+  { image: tomatoSaladImg, alt: "A delicious tomato salad" },
+];
+
+function FoodSlide() {
+  const [imageIndex, setImageIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setImageIndex((prev) => (prev < allFoodsImage.length - 1 ? prev + 1 : 0));
+    }, 5000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <div className=" relative overflow-hidden w-[420px] h-[340px] md:w-[500px] md:h-[400px] rounded-xl">
+      {allFoodsImage.map((item, index) => (
+        <Image
+          className={` absolute transition-all duration-500 ease-in-out  w-[420px] h-[340px] md:w-[500px] md:h-[400px] rounded-xl shadow-xl scale-105  ${
+            index === imageIndex
+              ? " opacity-100 scale-100 rotate-0 translate-x-0  "
+              : "opacity-0 scale-110 -rotate-6 -translate-x-1"
+          } `}
+          src={item.image}
+          width={800}
+          height={800}
+          alt="burger image"
+        />
+      ))}
+    </div>
+  );
 }
 
-export default Slide
+export default FoodSlide;
--- components/main/MainSection.tsx
+++ components/main/MainSection.tsx
@@ -1,18 +1,12 @@
 import Link from "next/link";
-import burgerImg from "@/assets/burger.jpg";
-import Image from "next/image";
+import FoodSlide from "./MainFoodsSlide";
 
 function MainSection() {
   return (
     <div className="flex flex-col px-4 mt-10 space-y-10  md:mt-0 md:-translate-y-6 md:px-36">
       <div className="flex flex-col  space-y-6 items-center md:flex-row md:items-start md:justify-center md:space-y-0 md:space-x-6">
-        <Image
-          className="w-[420px] h-[340px] md:w-[500px] md:h-[400px] rounded-xl shadow-2xl"
-          src={burgerImg}
-          width={600}
-          height={600}
-          alt="burger image"
-        />
+
+        <FoodSlide />
 
         <div className="text-center space-y-5 px-6 md:text-start md:p-10 md:max-w-[500px] ">
           <h2 className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-slate-500 to-pink-800 text-3xl tracking-wide">
@@ -51,7 +45,9 @@ function MainSection() {
           </p>
         </div>
         <div className="text-white md:-translate-y-10 shadow-[0_0_5px_0_pink] rounded-xl p-4 space-y-8 tracking-wider md:max-w-[600px]">
-          <h2 className="text-2xl md:text-3xl font-semibold">Why NextLevel Food?</h2>
+          <h2 className="text-2xl md:text-3xl font-semibold">
+            Why NextLevel Food?
+          </h2>
           <p className="md:text-xl opacity-50">
             NextLevel Food is a platform for foodies to share their favorite
             recipes with the world. It's a place to discover new dishes, and to
