da3514a240863bb9eb2852c3197820292a7f55b3
yusufdgzl <yusuf.dg13@gmail.com>
2024-04-13 21:44:28 +0300

    share page configured

--- app/meals/share/page.tsx
+++ app/meals/share/page.tsx
@@ -9,7 +9,8 @@ export default function SharePage() {
           Or any other meal you feel needs sharing!
         </h2>
       </div>
-      <form className=" flex flex-col mt-10 px-6 md:px-0 space-y-4  md:pl-36 max-w-[900px]">
+
+      <form className=" flex flex-col  px-6 py-10 md:px-0 space-y-4  md:pl-36 max-w-[900px]">
         <div className="flex space-x-4 ">
           <div className="flex flex-col w-full space-y-2">
             <label className="text-gray-400 font-semibold" htmlFor="name">
@@ -18,11 +19,46 @@ export default function SharePage() {
             <input className="input" type="text" id="name" name="name" />
           </div>
           <div className="flex flex-col w-full space-y-2">
-            <label className="text-gray-400 font-semibold" htmlFor="name">
+            <label className="text-gray-400 font-semibold" htmlFor="email">
               YOUR EMAIL
             </label>
-            <input className="input" type="text" id="email" name="email" />
+            <input className="input" type="email" id="email" name="email" />
           </div>
+        </div>
+
+        <div className="flex flex-col w-full space-y-2">
+          <label className="text-gray-400 font-semibold" htmlFor="title">
+            TITLE
+          </label>
+          <input className="input" type="text" id="title" name="title" />
+        </div>
+
+        <div className="flex flex-col w-full space-y-2">
+          <label className="text-gray-400 font-semibold" htmlFor="shortSummary">
+            SHORT SUMMARY
+          </label>
+          <input
+            className="input"
+            type="text"
+            id="shortSummary"
+            name="shortSummary"
+          />
+        </div>
+
+        <div className="flex flex-col w-full space-y-2">
+          <label className="text-gray-400 font-semibold" htmlFor="instructions">
+            INSTRUCTIONS
+          </label>
+          <textarea
+            className="input h-40 "
+            id="instructions"
+            name="instructions"
+          />
+        </div>
+        <div className="w-full flex justify-end">
+          <button className="w-full md:w-[200px] rounded-md border text-pink-600 border-pink-600 hover:bg-pink-600 hover:text-white transition-colors duration-300  py-2">
+            Share Meal
+          </button>
         </div>
       </form>
     </>
