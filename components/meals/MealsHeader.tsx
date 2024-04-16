3e7927232535b7064d2c7cd4d9280d407d196882
yusufdgzl <yusuf.dg13@gmail.com>
2024-04-16 21:29:28 +0300

    search-icon added

--- components/meals/MealsHeader.tsx
+++ components/meals/MealsHeader.tsx
@@ -1,13 +1,30 @@
 import Link from "next/link";
+import MealsSearch from "./MealsSearch";
 
 function MealsHeader() {
   return (
     <div className="flex flex-col items-center md:items-start text-white space-y-6 py-6 px-6 md:px-36">
-      <h1 className="flex text-2xl font-bold justify-center md:justify-start md:text-5xl">
-        Delicious meals, created <p className="px-2 text-pink-700">by you</p>
-      </h1>
-      <h2 className="text-center md:text-left text-lg">Choose your favorite recipe and cook it yourself. It is easy and fun!</h2>
-      <Link href="/meals/share" className="py-1 px-4 rounded-lg bg-pink-700 text-xl  " >Share Your Favorite Recipe</Link>
+      <div className="flex md:items-center md:w-full md:justify-between">
+        <h1 className="flex text-2xl font-bold justify-center md:justify-start md:text-5xl">
+          Delicious meals, created <p className="px-2 text-pink-700">by you</p>
+        </h1>
+        <div className="hidden md:flex">
+          <MealsSearch />
+        </div>
+      </div>
+
+      <h2 className="text-center md:text-left text-lg">
+        Choose your favorite recipe and cook it yourself. It is easy and fun!
+      </h2>
+      <Link
+        href="/meals/share"
+        className="py-1 px-4 rounded-lg bg-pink-700 text-xl hover:bg-pink-600  "
+      >
+        Share Your Favorite Recipe
+      </Link>
+      <div className="md:hidden">
+        <MealsSearch />
+      </div>
     </div>
   );
 }
---
+++ components/meals/MealsSearch.tsx
@@ -0,0 +1,11 @@
+import SearchIcons from "../ui/icons/SearchIcons";
+
+export default function MealsSearch() {
+  
+    return (
+    <div className="">
+     <SearchIcons/>
+      <input placeholder="Search the recipes" type="text" id="search" name="search" />
+    </div>
+  );
+}
